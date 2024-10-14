fae233d4f3c7e7120abcbba05c0bf60bbb84ad6a
Michael <hoangnv725@gmail.com>
2024-10-14 09:44:57 +0700

    up

--- Nam_2/Ki_3/CTDL&GT/12102024/moiconduongvekhong.cpp
+++ Nam_2/Ki_3/CTDL&GT/12102024/moiconduongvekhong.cpp
@@ -3,59 +3,50 @@ using namespace std;
 
 struct node {
 	int elem;
-	vector<node*> child;
-	// Tao de quy ra cay
-	node(int n) {
+	node *left, *right;
+	node (int n) {
 		elem = n;
-		for (int a = 1; a*a <= n; a++) {
-			if (n%a == 0) child.push_back(new node((a-1)*(n/a+1)));
-		}
-	}
+		if (n % 2 == 0 && n > 4) {
+			left = new node(n/2-2);
+			right = new node(n/2+2);
+		} 
+		else left=right=NULL;
+	}	
 };
 
 void preorder(node *T) {
+	if(!T) return;
 	cout << T->elem << " ";
-	for (auto c:T->child) preorder(c);
+	preorder(T->left);
+	preorder(T->right);
 }
 
 void inorder(node *T) {
-	if(T->child.size()) {
-		inorder(T->child[0]);
-	}
+	if(T == 0) return;
+	inorder(T->left);
 	cout << T->elem << " ";
-	if(T->child.size() > 1) {
-		for (int i = 1; i < T->child.size(); i++)
-		inorder(T->child[i]);
-	}
+	inorder(T->right);
 }
 
 void postorder(node *T) {
-	for (auto c:T->child) postorder(c);
-	cout << T->elem << " ";
+	if (T == 0) return;
+	postorder(T->left);	
+	postorder(T->right);
+	cout << T->elem << " ";		
 }
 
-int sum(node *T) {
-	int s = T->elem;
-	for (auto c:T->child) s+=sum(c);
-	return s;
+void draw(node *T, string prompt="\n") {
+	if (T == 0) return;
+	draw(T->left, prompt+"\t");
+	cout << prompt << T->elem;
+	draw(T->right, prompt+"\t");
 }
-
-int count(node *T, int k = 0) {
-	if (T->elem == k) return 1;
-	if (T->elem < k) return 0;
-	int s = 0;
-	for (auto c:T->child) s+=count(c, k);
-	return s;
-} 
 
 int main() {
-	int n;
-	cin >> n;
-	node *T = new node(n);
-	cout << "\nTien thu tu : "; preorder(T);
-	cout << "\nTrung thu tu : "; inorder(T);
-	cout << "\nHau thu tu : "; postorder(T);
-	cout << "\nTong cay : Sum = " << sum(T);
-	cout << "\nDem so 0 : " << count(T, 0);
-	cout << "\nDem so 10 : " << count(T, 10);
+	node *T = new node(60);
+	cout << "\npre : "; preorder(T);
+	cout << "\nin : "; inorder(T);
+	cout << "\npost : "; postorder(T);
+	cout << "\n\nVe : \n\n"; draw(T);
 }
+
